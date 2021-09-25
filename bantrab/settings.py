2012c3014a280a187518f0f850d053011ba1db68
Santos Martín García <santos.martingarca2@gmail.com>
2021-09-25 01:03:02 -0600

    ADD: Configuracion de la api interna

--- bantrab/settings.py
+++ bantrab/settings.py
@@ -133,9 +133,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
      ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+API_IMAGENES='https://apitest-bt.herokuapp.com/api/v1/imagenes'
