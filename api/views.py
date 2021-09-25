0fd3c9c8da04fb1787574b252e9665d072941927
Santos Martín García <santos.martingarca2@gmail.com>
2021-09-25 01:04:07 -0600

    ADD: Metodo create(POST) para subir las imagenes

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
-from rest_framework import permissions
+import os
+import base64
+import requests
+from django.conf import settings
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from api.serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -13,10 +17,11 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class CustomAuthToken(ObtainAuthToken):
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
@@ -29,3 +34,57 @@ class CustomAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'email': user.email
         })
+
+
+class ImagesViewset(viewsets.ViewSet):
+    """
+   API endpoint: Permite listar o subir imagenes a una api interna.
+   """
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request):
+        try:
+            data = request.data
+            final_endpoint = settings.API_IMAGENES
+
+            #   Nombre requerido
+            file_name = data.get('file_name', None)
+            if file_name is None:
+                return Response({'detail': 'El nombre de la imagen es requerido'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            #   VALIDACION DE IMAGEN
+            file = data.get('file', None)
+            if file is None:
+                return Response({'detail': 'La imagen es requerida'},
+                                status=status.HTTP_400_BAD_REQUEST)
+            extension = os.path.splitext(file.name)[1].lower()
+
+            if not extension.lower() in ['.jpg', '.png', '.jpeg', '.gif']:
+                return Response({'detail': 'El archivo no es una imagen'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            #   CONVERTIR IMAGEN A BASE64
+            encoded_string = base64.b64encode(file.read()).decode('utf-8')
+
+            body = {
+                "imagene": {
+                    "nombre": file_name,
+                    "base64": encoded_string
+                }
+            }
+
+            response = requests.post(final_endpoint, body, headers={
+                'user': 'User123',
+                'password': 'Password123'
+            })
+
+            if not response.status_code in [200, 201, 202]:
+                return Response({'detail': f'Error en la api interno: {str(response.reason)}'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            return Response({}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print('ERROR: ', str(e))
+            return Response({'detail': 'No fue posible subir la imagen, intenta mas tarde'},
+                            status=status.HTTP_400_BAD_REQUEST)
